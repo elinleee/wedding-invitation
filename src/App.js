629f743d04cba24e36ecc68a77bb0637d4ce300a
elinleee <lleeyj94@gmail.com>
2025-03-13 21:59:46 +0900

    add nodemon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DARANGJI <code>src/App.js</code> and save to reload.
+          다솔아 사랑행!!!!!! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
