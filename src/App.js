bd49fa7586581b854f9d3042551465a411c37c4b
elinleee <lleeyj94@gmail.com>
2025-03-18 16:25:26 +0900

    apply Container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
+import { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Container from './layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          다랑지 청첩장입니다 :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      
+    </Container>
   );
 }
 
