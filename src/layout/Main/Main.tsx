e002b40fc7d0b9b051596222f6e4cbfe66a029a7
youngji <youngji@youngjiui-MacBookPro.local>
2025-04-27 23:13:04 +0900

    change name

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -7,7 +7,18 @@ const Main = () => {
   return (
     <div>
       <MainImg src={mainImg} />
-      <MainTitle>{greeting.title}</MainTitle>
+
+      <NameContainer>
+        <NameBox>
+          <GroomBride>GROOM</GroomBride>
+          <Name>이다솔</Name>
+        </NameBox>
+        <NameBox>
+          <GroomBride>BRIDE</GroomBride>
+          <Name>이영지</Name>
+        </NameBox>
+      </NameContainer>
+      {/* <MainTitle>{greeting.title}</MainTitle> */}
       <SubTitle>{greeting.eventDetail}</SubTitle>
     </div>
   );
@@ -17,21 +28,53 @@ export default Main;
 
 const MainImg = styled.img`
   // border-radius: 200px 200px 0 0;
-  width: 90%;
+  width: 100%;
   max-width: 450px;
   padding-top: 20px;
 `;
 
+const NameContainer = styled.div`
+  display: flex;
+  // width: 80vw;
+  margin: 0 auto;
+  // height: 100vh;
+`
+
+const NameBox = styled.div`
+  flex: 1;
+  // display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const GroomBride = styled.div`
+  color: pink;
+`
+
+const Name = styled.div`
+  // color: #22ffff;
+  font-size: 1.8rem;
+  letter-spacing: 10px;
+`
+
+const AndTitle = styled.p`
+  // font-family: HSSanTokki20-Regular, serif;
+  font-size: 1.2rem;
+  color: #2F2120;
+  line-height: 120%;
+  white-space: pre-line;
+`;
+
 const MainTitle = styled.p`
-  font-family: HSSanTokki20-Regular, serif;
-  font-size: 2rem;
+  // font-family: HSSanTokki20-Regular, serif;
+  font-size: 1.7rem;
   color: #2F2120;
   line-height: 120%;
   white-space: pre-line;
 `;
 
 const SubTitle = styled.p`
-  font-size: 1.1rem;
+  // font-size: 1.1rem;
   color: #2F2120;
   line-height: 140%;
   white-space: pre-line;
